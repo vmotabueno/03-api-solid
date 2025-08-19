43cda14eb18cf8a3abab55a4131847991d853bf7
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-08-19 10:32:58 -0300

    Configurando ESlint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app.js"
-import { env } from "./env/index.js"
+import { app } from './app.js'
+import { env } from './env/index.js'
 
-app.listen({
-  host: "0.0.0.0",
-  port: env.PORT,
-}).then(() => {
-  console.log(`🚀 HTTP Server Running on port ${env.PORT}`)
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 HTTP Server Running on port ${env.PORT}`)
+  })
