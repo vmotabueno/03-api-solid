174839648e870414dea906a8257420fe2953bc8f
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-08-20 16:59:35 -0300

    Caso de Uso e Design Patterns - Caso de uso de registro

---
+++ src/services/register.ts
@@ -0,0 +1,34 @@
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+
+interface registerUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: registerUseCaseRequest) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('E-mail already exists')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+}
